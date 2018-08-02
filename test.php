436337f395a8f68de9b5fd68bb55a6e276312867
王飞 <wangfei@SFdeMacBook-Pro-96.local>
2018-08-02 16:33:28 +0800

    增添测试脚本

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+/** 测试 */
+include './IdWorker.php';
+
+function test() {
+    $idWorker = new SnowflakeIdWorker(0, 0);
+    for ($i = 0; $i < 1000; $i++) {
+        $id = $idWorker->nextId();
+        // echo $id . PHP_EOL;
+    }
+}
+
+test();
